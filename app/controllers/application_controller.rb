24cb5555e8faaf1bb4e468499fdd49b6627aed45
Your Name <you@example.com>
2019-01-29 23:33:50 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
+  end
+
+  # => index_page
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # => new_page
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  # => create_page
+  post "/articles" do
+     @article = Article.create(params)
+     redirect to "/articles/#{ @article.id }"
+   end
+
+
+  # =>  show_one_instance
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # => edit_page
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # => update_page
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  # => DELETE
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+
+    redirect to "/articles"
   end
 end
